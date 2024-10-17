2aeafde080b6a0559e82ecf2e47a24473abe24b9
Jet Xu <Voldemort.xu@foxmail.com>
2024-10-17 22:01:23 +0800

    arragne logger.py (#23)
    Co-authored-by: Jet Xu <voldemort.xu@foxmai.com>

--- llama_github/logger.py
+++ llama_github/logger.py
@@ -13,16 +13,3 @@ def configure_logging(level=logging.INFO, handler=None):
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
         logger.addHandler(ch)
-
-## Logging Configuration
-# By default, `llama_github` does not configure logging to avoid interfering with your application's logging settings. If you want to see log outputs from `llama_github`, you can configure logging as follows:
-
-# ```python
-# import logging
-# import llama_github
-
-# # Configure the library's logger
-# llama_github.configure_logging(level=logging.INFO)
-
-# # Now you can see log outputs
-# logger = logging.getLogger('llama_github')
